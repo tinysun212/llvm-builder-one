60dafb25c510023c6df8249960365240b0c7f8f1
Han Sang-jin <tinysun.net@gmail.com>
2018-04-21 08:07:46 +0900

    Create after_build_msvc.bat

---
+++ after_build_msvc.bat
@@ -0,0 +1,4 @@
+rem after_build
+
+ 7z a -so -ttar -r dummy *.obj *.lib *.inc *.tmp *.res *.manifest include bin/clang-tblgen.exe bin/llvm-tblgen.exe .ninja_deps .ninja_log | 7z a -si -tgzip -bd -bso0 swift_llvm_cache.tar.gz
+ MOVE swift_llvm_cache.tar.gz %APPVEYOR_BUILD_FOLDER%
