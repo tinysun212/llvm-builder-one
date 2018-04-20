74bf3cd1731628ec1089692417c4bef2253fb29b
Han Sang-jin <tinysun.net@gmail.com>
2018-04-21 08:03:25 +0900

    Create after_build_cygwin.bat

---
+++ after_build_cygwin.bat
@@ -0,0 +1,6 @@
+rem after_build
+
+rem Make cache to continue in llvm-builder
+ find . -name *.o -o -name *.obj -o -name *.a -o -name *.inc -o -name *.tmp > tar_file.list
+ tar zcvf swift_llvm_cache.tar.gz -T tar_file.list include bin/clang-tblgen.exe bin/llvm-tblgen.exe .ninja_deps .ninja_log
+ mv swift_llvm_cache.tar.gz %APPVEYOR_BUILD_FOLDER%
